a153851784b644c1aff6179bf24405eb7a68b896
Leonardo2 <leo.decristo@hotmail.com>
2024-07-13 12:42:28 -0300

    Calcula a idade baseado na data completa, d, m, a

--- Pessoa.cpp
+++ Pessoa.cpp
@@ -8,9 +8,16 @@ typedef struct Pessoa
     int idade;
 }Pessoa;
 
-int calculaIdade (Pessoa p, int anoAt)
+int calculaIdade (Pessoa p, int diaAt, int mesAt, int anoAt)
 {
     int idd = anoAt - p.ano;
+    if (mesAt < p.mes)
+        idd--;
+    else if (mesAt == p.mes && diaAt < p.dia)
+    {
+        idd--;
+    }
+    return idd;
 }
 
 int main ()
@@ -25,9 +32,10 @@ int main ()
     Newton.mes = 1;
     Newton.ano = 1643;
 
-    Einstein.idade = calculaIdade(Einstein, 2024);
-    Newton.idade = calculaIdade(Newton, 2024);
+    Einstein.idade = calculaIdade(Einstein, 13, 7, 2024);
+    Newton.idade = calculaIdade(Newton, 13, 7, 2024);
 
     printf ("A idade de Einstein seria: %d anos\n", Einstein.idade);
     printf ("A idade de Newton seria: %d anos\n", Newton.idade);
+    return 0;
 }
